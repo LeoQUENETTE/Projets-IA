a6156015ecb0881a5f050db2cd980386f79cb538
leoQUENETTE <leoettag@gmail.com>
2025-08-17 21:51:29 +0200

    OpenCV projects deleted

--- ComputerVision/projet1/convert_img_bit.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-if __name__ == "__main__":
-    image = cv2.imread("mon_image.png",cv2.IMREAD_GRAYSCALE)
-     if image is None:
-        print("Erreur : impossible de charger l'image")
-        exit()
